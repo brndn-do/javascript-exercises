eaed3f8beb5ec0bf336c12adaeb7774bf768ba76
brndn-do <dobrandon05@gmail.com>
2024-07-06 20:05:39 -0500

    Finish 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    for (argument of arguments) {
+        array = array.filter(function(val) {
+            return val !== argument;
+        });
+    }
+    return array;
 };
 
 // Do not edit below this line
