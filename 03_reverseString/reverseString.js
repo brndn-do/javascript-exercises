fd33ff580234715ebdf28147c0234c0f985f539d
brndn-do <dobrandon05@gmail.com>
2024-07-06 19:50:25 -0500

    Finish 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let list = str.split("");
+    list = list.toReversed();
+    let res = "";
+    for (substr of list) {
+        res += substr;
+    }
+    return res;
 };
 
 // Do not edit below this line
